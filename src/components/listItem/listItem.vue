dbea540951ec5009130acbb79e38be41d7618325
shiga <shigachen@outlook.com>
2017-07-04 18:56:22 +0800

    卡片完成

--- src/components/listItem/listItem.vue
+++ src/components/listItem/listItem.vue
@@ -4,32 +4,32 @@
       <img :src="wineData.pic_url" :data-link="wineData.h5_url" @click.prevent="linkClick"/>
       <!-- <a href="javascript:;" :style="`background-image:'url(${wineData.pic_url})'`"></a> -->
     </div>
-    <div class="introduce fr">
+    <div class="introduce">
       <h4>{{wineData.name.name_ch}}  {{wineData.name.name_en}}</h4>
       <!--价格展示-->
       <div class="price" v-show="wineData.price || wineData.buy_info.length">
-        <span v-show="wineData.price">{{wineData.price}}</span>
+        <span v-show="wineData.price">￥{{wineData.price}}.00元</span>
         <span v-show="wineData.buy_info.length" >商家：<em v-for="item in wineData.buy_info">{{item.retailer_name}}/{{item.price}}元</em> </span>
       </div>
       <!--地理-->
       <div class="geography">
         <ul>
-          <li class="guojia" >
+          <li class="guojia" v-if="wineData.country.name_ch">
             <a href="javascript:;">
-              <i class="iconfont"></i>{{wineData.country.name_ch}} / {{wineData.country.name_en}}
+              <i class="iconfont">&#xe606;</i>{{wineData.country.name_ch}} / {{wineData.country.name_en}}
             </a>
           </li>
-          <li class="chanqu">
+          <li class="chanqu" v-if="wineData.region.name_ch">
             <a href="javascript:;">
               <i class="iconfont">&#xe603;</i>{{wineData.region.name_ch}} / {{wineData.region.name_en}}
             </a>
           </li>
-          <li class="zichanqu" v-show="wineData.sub_region">
+          <li class="zichanqu" v-show="wineData.sub_region" v-if="wineData.sub_region.name_ch">
             <a href="javascript:;">
               <i class="iconfont">&#xe61a;</i>{{wineData.sub_region.name_ch}} / {{wineData.sub_region.name_en}}
             </a>
           </li>
-          <li class="jiuzhuang" v-show="wineData.winery">
+          <li class="jiuzhuang" v-show="wineData.winery" v-if="wineData.winery.name_ch">
             <a href="javascript:;" >
               <i class="iconfont">&#xe60b;</i>{{wineData.winery.name_ch}} / {{wineData.winery.name_en}}
             </a>
@@ -37,46 +37,44 @@
         </ul>
       </div>
       <!--葡萄品种-->
-      <div class="variety">
-        <span class="fl">葡萄品种 :</span>
+      <div class="variety" v-show="wineData.grape">
+        <span class="fl"><i class="iconfont">&#xe613;</i>葡萄品种 :</span>
         <ul>
           <li class="pz-1" v-for="item in wineData.grape">
             <a href="javascript:;">
-              <i class="iconfont">&#xe613;</i>{{item.name_ch}} / {{item.name_en}}
+              {{item.name_ch}} / {{item.name_en}}
             </a>
           </li>
-          <!-- <li class="pz-2"><a href="javascript:;"><i class="iconfont">&#xe613;</i>小味儿多 / english</a></li>
-          <li class="pz-3"><a href="javascript:;"><i class="iconfont">&#xe613;</i>品丽珠 / english</a></li> -->
         </ul>
       </div>
       <!--类型-->
-      <div class="type">
-        <span class="fl">酒类型 :</span>
+      <div class="type" v-if="wineData.wine_type.name_ch">
+        <span class="fl"><i class="iconfont">&#xe60d;</i>酒类型 :</span>
         <ul>
-          <li class=""><a href="javascript:;"><i class="iconfont">&#xe60d;</i>
+          <li class=""><a href="javascript:;">
           {{wineData.wine_type.name_ch}} / {{wineData.wine_type.name_en}}</a>
           </li>
         </ul>
       </div>
       <!--等级-->
-      <div class="grade">
-        <span class="fl">等级：</span>
+      <div class="grade" v-if="wineData.winery_level.name_ch">
+        <span class="fl"><i class="iconfont">&#xe601;</i>等级：</span>
         <ul>
-          <li><a href=""><i class="iconfont">&#xe601;</i>{{wineData.winery_level.name_ch}} / {{wineData.winery_level.name_en}}</a></li>
+          <li><a href="">{{wineData.winery_level.name_ch}}/{{wineData.winery_level.name_en}}</a></li>
         </ul>
       </div>
       <!--场合-->
-      <div class="situation">
-        <span class="fl">场合：</span>
+      <div class="situation" v-if="wineData.occasion.name_ch">
+        <span class="fl"><i class="iconfont">&#xe605;</i>场合：</span>
         <ul>
-          <li><a href=""></a><i class="iconfont">&#xe605;</i>{{wineData.occasion.name_ch}}</li>
+          <li><a href=""></a>{{wineData.occasion.name_ch}}</li>
         </ul>
       </div>
       <!--配餐-->
-      <div class="pantry">
-        <span class="fl">配餐：</span>
+      <div class="pantry" v-if="wineData.recipe_pair.name_ch">
+        <span class="fl"><i class="iconfont">&#xe611;</i>配餐：</span>
         <ul>
-          <li><i class="iconfont">&#xe611;</i>{{wineData.recipe_pair.name_ch}}</li>
+          <li>{{wineData.recipe_pair.name_ch}}</li>
         </ul>
       </div>
       <!--更多-->
@@ -118,74 +116,93 @@ export default {
 }
 </script>
 <style scoped lang="less" rel="stylesheet/less">
+  /*字体图标*/
+  @font-face {
+    font-family: 'iconfont';
+    src: url('../../../static/iconfont/iconfont.eot');
+    src: url('../../../static/iconfont/iconfont.eot?#iefix') format('embedded-opentype'),
+    url('../../../static/iconfont/iconfont.woff') format('woff'),
+    url('../../../static/iconfont/iconfont.ttf') format('truetype'),
+    url('../../../static/iconfont/iconfont.svg#iconfont') format('svg');
+  }
+  .iconfont{
+    font-family:"iconfont" !important;
+    font-size:16px;font-style:normal;
+    -webkit-font-smoothing: antialiased;
+    -webkit-text-stroke-width: 0.2px;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  i{color:#BE344E;margin-right: 4px;}
+  li{word-break:keep-all;/* 不换行 */
+    white-space:nowrap;/* 不换行 */
+    overflow:hidden;/* 内容超出宽度时隐藏超出部分的内容 */
+    text-overflow:ellipsis;/* 当对象内文本溢出时显示省略标记(...) ；需与overflow:hidden;一起使用。*/
+  }
 
 .list-item{
-  width: 100%;
-  height: 300px;
-  margin: 20px auto;
-  padding: 1%;
-  border: 1px solid #ccc;
-  > .pic {
-    position: relative;
-    min-width: 18%;
-    height: 100%;
-    padding: 0.5%;
-    /*margin-right: 3%;*/
-    > img {
-      position: absolute;
+  width:100%;
+  /*height:320px;*/
+  margin:auto;
+  /*margin-left: 3%;*/
+  /*padding-top: 20px;*/
+  padding: 2%;
+  /*padding-bottom: 20px;*/
+  border-top: 1px solid #ccc;
+  .pic{
+    width:180px;
+    height:8.2%;
+    padding: 2px;
+    overflow: hidden;
+    position: absolute;
+    >img{
       display: block;
-      left: 0;top:0;
-      width: 90%;
+      width: 100%;
       height: 100%;
-      // background:no-repeat center center;
-      // background-size: cover;
     }
   }
-  > .introduce {
+  .introduce{
+    margin-left: 220px;
     width: 80%;
     height: 100%;
-    padding: 5px;
+    /*padding: 5px;*/
     span{
-      color: #646464;
+      color: #333;
     }
     li {
       float: left;
-      color:#000;
-    }
-    i {
-      color: #c03c4e;
+      >a{
+        color:#333;
+        cursor: pointer;
+      }
     }
     > h4 {
       display: block;
       width: 100%;
       height: 24px;
+      font-weight: normal;
       overflow: hidden;
+      font-size: 20px;
     }
+    /*div{*/
+      /*margin-top: 8px;*/
+    /*}*/
     .price {
-      margin-top: 5px;
-      margin-bottom: 8px;
       span:nth-of-type(1) {
         margin-right: 15%;
         font-size: 120%;
-        font-weight: bold;
-        color: red;
+        color:#BE344E;
       }
       span:nth-of-type(2) {
         font-size: 13px;
       }
-      a {
-        font-size: 14px;
-      }
-    }
-    .geography.variety, .type, .grade, .situation, .pantry {
-      margin-top: 8px;
     }
     .geography {
       width: 100%;
       height: 25px;
+      margin-top: 10px;
       > ul {
         > li {
-          width: 24%;
+          width: 22%;
           height: 100%;
           margin-right:1% ;
         }
@@ -194,6 +211,11 @@ export default {
     .variety {
       width: 100%;
       height: 25px;
+      margin-top: 10px;
+      word-break:keep-all;/* 不换行 */
+      white-space:nowrap;/* 不换行 */
+      overflow:hidden;/* 内容超出宽度时隐藏超出部分的内容 */
+      text-overflow:ellipsis;/* 当对象内文本溢出时显示省略标记(...) ；需与overflow:hidden;一起使用。*/
       ul {
         > li {
           width: 24%;
@@ -205,6 +227,7 @@ export default {
     .type {
       width: 100%;
       height: 25px;
+      margin-top: 10px;
       ul {
         > li {
           width: 24%;
@@ -216,17 +239,20 @@ export default {
     .grade {
       width: 100%;
       height: 25px;
+      margin-top: 10px;
     }
     .situation {
       width: 100%;
       height: 25px;
+      margin-top:10px;
     }
     .pantry {
       width: 100%;
       height: 25px;
+      margin-top: 10px;
     }
     .more{
-      margin-top: 8px;
+      margin-top: 10px;
       ul{
         li{
           margin-right: 8%;
@@ -235,5 +261,4 @@ export default {
     }
   }
 }
-
 </style>
